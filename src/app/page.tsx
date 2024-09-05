55db09302503ac40ddbf554679085769e108320c
ashidthanikkal <ashidthanikkal@gmail.com>
2024-09-05 06:54:17 +0530

    Home2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,6 +133,79 @@ export default function Home() {
         </div>
       </div>
 
+      {/* about us */}
+
+      <div className="aboutus px-5">
+        <div className="aboutus_parent flex-col-reverse flex md:flex-row items-center">
+
+          <div className="aboutus_child1 md:w-1/2">
+            <div className="grid grid-rows-3 grid-flow-col gap-4">
+              <div className="col-span-2 bg-lime-600 rounded-lg">01</div>
+              <div className="row-span-2 col-span-2 bg-blue-600 rounded-lg">02</div>
+              <div className="row-span-3 bg-amber-900 rounded-lg">03</div>
+            </div>
+          </div>
+
+          <div className="aboutus_child2 md:w-1/2">
+            <div className="about_overview  p-10 ">
+              <div className="overview1 w-full md:flex-col flex-row">
+                <div className="flex  md:flex-row-reverse items-center md:justify-end gap-4">
+                  <div className="w-10 border-t-2 border-[#DC272F] flex justify-center text-[8px] leading-[8px] md:leading-[12px] md:text-[12px] lg:leading-[28.8px] lg:text-[18px] mt-2 text-[#DC272F]"></div>
+                  <div>
+                    <p className="font-normal text-[8px] leading-[8px] md:leading-[12px] md:text-[12px] lg:leading-[28.8px] lg:text-[18px] mt-2 text-[#DC272F]">
+                      ABOUT US
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex md:flex-col flex-row justify-between">
+                  <h2 className="text-[#04374B] mt-3 font-bold md:font-semibold  leading-[16.8px] text-[14px] md:leading-[16.8px] md:text-[14px] lg:leading-[52px] lg:text-[40px] ">
+                    Our Real Commitment Reaches Beyond Gas & Oil Company.
+                  </h2>
+                  <div>
+                    {" "}
+                    <IoIosArrowForward className="text-red-600 nd:w-10 md:hidden" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="overview2 w-full ">
+                <p className="font-normal text-[8px] leading-[12.8px] md:leading-[22.4px] md:text-[14px] lg:leading-[28.8px] lg:text-[17px] text-[#DC272F] mt-2 text-left">
+                  UE is a private educational and training services provider,
+                  following huge educational and skill needs among our people that
+                  make it difficult for the country and nation to develop and cope
+                  with the rest of the world.
+                </p>
+
+                <p className="font-normal text-[8px] leading-[12.8px] md:leading-[22.4px] md:text-[14px] lg:leading-[28.8px] lg:text-[17px] mt-2 text-left">
+                  UE is specialized in educational and training services, ranging from
+                  language education, business skills development, and special
+                  technical and vocational training services.
+                  <span className="hidden sm:inline">
+                    We have a blend of products and services for all public, private,
+                    non-governmental, educational and individual customers who are
+                    looking for excellent educational solutions.
+                  </span>
+                </p>
+
+                <button
+                    type="button"
+                    className="text-white bg-[#14516A] hover:bg-[#14516A] w-48 mt-7 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 hidden md:block"
+                  >
+                    View Our Services
+                  </button>
+
+
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+
+
 
     </main>
   );
